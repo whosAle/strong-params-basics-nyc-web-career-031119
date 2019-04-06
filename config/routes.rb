d87f8e200accbee78a0a24898b4b12c6b87b1f8e
Alejandro <abelgrave95@gmail.com>
2019-04-06 23:03:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :post
+  resources :post, only: [:index,:show, :create, :new]
 end
